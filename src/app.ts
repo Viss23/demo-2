2f44a1ed61108f44f30ab5728129f1e4bf139ed4
viss23 <oleh.i.nosal@gmail.com>
2022-07-20 19:37:34 +0300

    controller

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express, { Express } from "express";
+import { Server } from "http";
+import { LoggerService } from "./logger/logger.service.js";
+import { UserController } from "./users/users.controller.js";
+
+export class App {
+  app: Express;
+  server: Server;
+  port: number;
+  logger: LoggerService;
+  userController: UserController;
+
+  constructor(logger: LoggerService, userController: UserController) {
+    this.app = express();
+    this.port = 8000;
+    this.logger = logger;
+    this.userController = userController;
+  }
+
+  userRoutes() {
+    this.app.use("/users", this.userController.router);
+  }
+
+  public async init() {
+    this.userRoutes();
+    this.server = this.app.listen(this.port, () => {
+      this.logger.log(`Server http://localhost:${this.port}`);
+    });
+  }
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,23 @@
+import { BaseController } from "../common/base.controller";
+import { LoggerService } from "../logger/logger.service";
+import { NextFunction, Response, Request } from "express";
+
+export class UserController extends BaseController {
+  constructor(logger: LoggerService) {
+    super(logger);
+
+    this.bindRoutes([
+      { path: "/register", method: "post", func: this.register },
+      { path: "/login", method: "get", func: this.login },
+    ]);
+  }
+
+  login(req: Request, res: Response, next: NextFunction) {
+    console.log("ss");
+    this.ok(res, "login");
+  }
+
+  register(req: Request, res: Response, next: NextFunction) {
+    this.ok(res, "register");
+  }
+}
