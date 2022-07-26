1570132d7ffd308c684c39cb235336d36c1df392
viss23 <oleh.i.nosal@gmail.com>
2022-07-26 13:41:53 +0300

    user service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,10 +8,15 @@ import 'reflect-metadata';
 import { IUserController } from './users.controller.interface';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
+import { User } from './user.entity';
+import { IUserService } from './users.service.interface';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
-	constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
+	constructor(
+		@inject(TYPES.ILogger) private loggerService: ILogger,
+		@inject(TYPES.UserService) private userService: IUserService,
+	) {
 		super(loggerService);
 
 		this.bindRoutes([
@@ -25,8 +30,15 @@ export class UserController extends BaseController implements IUserController {
 		next(new HTTPError(401, 'login error', 'login'));
 	}
 
-	register(req: Request<{}, {}, UserRegisterDto>, res: Response, next: NextFunction): void {
-		console.log(req.body);
-		this.ok(res, 'register');
+	async register(
+		{ body }: Request<{}, {}, UserRegisterDto>,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		const result = await this.userService.createUser(body);
+		if (!result) {
+			return next(new HTTPError(422, 'Such user already exist'));
+		}
+		this.ok(res, { email: result.email });
 	}
 }
---
+++ src/users/users.service.ts
@@ -0,0 +1,17 @@
+import { injectable } from 'inversify';
+import { User } from './user.entity';
+import { UserLoginDto } from './dto/user-login.dto';
+import { UserRegisterDto } from './dto/user-register.dto';
+import { IUserService } from './users.service.interface';
+
+@injectable()
+export class UserService implements IUserService {
+	async createUser({ email, name, password }: UserRegisterDto): Promise<User | null> {
+		const newUser = new User(email, name);
+		await newUser.setPassword(password);
+		return newUser;
+	}
+	async validateUser(dto: UserLoginDto): Promise<boolean> {
+		return true;
+	}
+}
